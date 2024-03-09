4d34d647535495900dbb459a7cbc030557bf691f
Azrioth <136956731+Azrioth@users.noreply.github.com>
2024-03-09 18:40:45 +0800

    Update test.js

--- Ver 3.0/test.js
+++ Ver 3.0/test.js
@@ -1,12 +1,35 @@
-/*
+//Install Command:
+//npm init
+//npm i express express-handlebars body-parser mongodb mongoose
+const express = require('express');
+const server = express();
+
+const bodyParser = require('body-parser');
+server.use(express.json()); 
+server.use(express.urlencoded({ extended: true }));
+
+const handlebars = require('express-handlebars');
+server.set('view engine', 'hbs');
+server.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+server.use(express.static('public'));
+
 const { MongoClient } = require('mongodb');
 const databaseURL = "mongodb://127.0.0.1:27017/";
 const mongoClient = new MongoClient(databaseURL);
-*/
 
-//const databaseName = "EspressoSelf";
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/EspressoSelf');
+
+const databaseName = "EspressoSelf";
+const collection1 = "cafe";
+const collection2 = "review";
+const collection3 = "user";
+
+//i dont know if we'll use mongoose right now, uncomment if needed
+
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1:27017/EspressoSelf');
 
 function errorFn(err){
     console.log('Error found. Check connection.');
@@ -15,45 +38,55 @@ function errorFn(err){
 function errorFunc(err){
     console.log('Error spotted. Collection making failed.')
 }
-// function successFn(res){
-//     console.log('Database query successful!');
-// }
+
+function successFn(res){
+    console.log('Database query successful!');
+}
  
-/*mongoClient.connect().then(function(con){
+mongoClient.connect().then(function(con){
     console.log("Attempt to create!");
     const dbo = mongoClient.db(databaseName);
-}).catch(errorFn);*/
+    //Will create a collection if it has not yet been made
+    dbo.createCollection(collection1)
+      .then(successFn).catch(errorFn);
+
+    dbo.createCollection(collection2)
+      .then(successFn).catch(errorFn);
+
+    dbo.createCollection(collection3)
+      .then(successFn).catch(errorFn);
+  }).catch(errorFn);
 
 
+//uncomment if needs to be used, mongoose isnt a hard requirement so i dont know if we'll use it right now
 
-
-const acctSchema = new mongoose.Schema({
-    username: { type: String },
-    email: { type: String },
-    password: { type: String }
-  }, { versionKey: false });
+// const acctSchema = new mongoose.Schema({
+//     username: { type: String },
+//     email: { type: String },
+//     password: { type: String }
+//   }, { versionKey: false });
   
-const acctModel = mongoose.model('acct', acctSchema);
+// const acctModel = mongoose.model('acct', acctSchema);
 
-const cafeSchema = new mongoose.Schema({
-    cafe_name: { type: String },
-    cafe_desc: { type: String },
-    cafe_owner: { type: String},
-    cafe_items: { type: Array, "default": []},
-    cafe_rating: {type: Number}
-}, { versionKey: false });
+// const cafeSchema = new mongoose.Schema({
+//     cafe_name: { type: String },
+//     cafe_desc: { type: String },
+//     cafe_owner: { type: String},
+//     cafe_items: { type: Array, "default": []},
+//     cafe_rating: {type: Number}
+// }, { versionKey: false });
 
-const cafeModel = mongoose.model('cafe', cafeSchema);
+// const cafeModel = mongoose.model('cafe', cafeSchema);
 
-const reviewSchema = new mongoose.Schema({
-    username: { type: String},
-    rating: { type: Number},
-    comment: { type: String},
+// const reviewSchema = new mongoose.Schema({
+//     username: { type: String},
+//     rating: { type: Number},
+//     comment: { type: String},
     
 
-}, { versionKey: false });
+// }, { versionKey: false });
 
-const reviewModel = mongoose.model('review', revSchema);
+// const reviewModel = mongoose.model('review', revSchema);
 
 server.get('/', function(req, resp){
 /*
@@ -62,7 +95,8 @@ refer to social media 4 (?)
 */ 
 });
 
-/*TODO: server.post, including navigating from one page to another */
+/*TODO: server.post, for each page(?) and login functionality, refer to i think 11 MongoDB file from NodeJS file samples for reference*/
+
 function finalClose(){
     console.log('Close connection at the end!');
     mongoose.connection.close();
